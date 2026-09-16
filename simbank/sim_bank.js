a237155f6df1d0fc6e862f3da5952ab382d27411
Шурик <shuriknilzori@gmail.com>
2026-09-16 15:07:03 +0300

    Релиз

--- simbank/sim_bank.js
+++ simbank/sim_bank.js
@@ -102,9 +102,7 @@ for (let i = 0; i < maxClients; i++) {
     const newpers = new Peoples()
     allpeople.push(newpers)
 }
-console.log(allpeople)
 allpeople.sort((a, b) => a.body.fio.localeCompare(b.body.fio, 'ru'))
-console.log(allpeople)
 //allpeople[0].bank.acc = genNumAcc(allpeople[0].body.pass, 12)
 //allpeople[0].bank.balance = 100000000
 
