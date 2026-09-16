6d2a5ea6c9f931be4ebbdf43a965d995c7e1d550
Шурик <shuriknilzori@gmail.com>
2026-09-16 12:55:41 +0300

    Релиз

--- SimBank/sim_bank.js
+++ SimBank/sim_bank.js
@@ -3,9 +3,12 @@ const data = [
         'Гавриловwа', 'Громовwа', 'Довженко', 'Егоровwа', 'Захаровwа', 'Ивакинwа', 'Каламбет', 'Коваленко', 'Луговmойwая', 'Мороз'],
     ['mАндрей', 'mСергей', 'wЛидия', 'wТатьяна', 'mЮрий', 'mНиколай', 'mМаксим', 'mМихаил', 'mОлег', 'wОльга'],
     ['Витальевич', 'Михайлович', 'Андреевич', 'Сергеевич', 'Юрьевич', 'Николаевич', 'Максимович', 'Олегович', 'Антонович', 'Владимирович']
-];
+]
 const uniletters = 'КЕНХВАРОСМТ'
-let curTime = new Date()
+const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
+    'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'
+]
+let hideCard = null, worldRunning = true, timespeed = 1, curTime = new Date()
 curTime.setHours(8, 0, 0, 0)
 
 class Peoples {
@@ -17,45 +20,51 @@ class Peoples {
     }
 
     chgMoney() {
-        if (curTime > this.evtm){
-        this.evtm.setMinutes(curTime.getMinutes() + Math.floor(Math.random()*50+10))
-const rndMoney = Math.floor(Math.random()*100+1)
-        if (Math.random() < 0.7&&this.cash>=rndMoney) { this.cash = this.cash - rndMoney 
-console.log(this.body.fio+' потратил '+rndMoney)} else{if (Math.random() < 0.3) { this.cash = this.cash + rndMoney 
-console.log(this.body.fio+' надыбал '+rndMoney)}       }
-        if (this.cash > 3000) {console.log(this.body.fio+' разбогател')}
-        if (this.cash < 100) {console.log(this.body.fio+' хочет кредит')}
-        this.cash = this.cash<0?0:this.cash
-}
-    } 
+        if (curTime > this.evtm) {
+            this.evtm.setMinutes(curTime.getMinutes() + Math.floor(Math.random() * 50 + 10))
+            const rndMoney = Math.floor(Math.random() * 100 + 1)
+            if (Math.random() < 0.7 && this.cash >= rndMoney) {
+                this.cash = this.cash - rndMoney
+                //console.log(this.body.fio + ' потратил ' + rndMoney)
+            } else {
+                if (Math.random() < 0.3) {
+                    this.cash = this.cash + rndMoney
+                    //console.log(this.body.fio + ' надыбал ' + rndMoney)
+                }
+            }
+            if (this.cash > 3000) { console.log(this.body.fio + ' разбогател') }
+            if (this.cash < 100) { console.log(this.body.fio + ' хочет кредит') }
+            this.cash = this.cash < 0 ? 0 : this.cash
+        }
+    }
 }
 
 class Persones {
     constructor(arrays) {
-        this.arrays = arrays;
-        this.multipliers = [];
-        let m = 1;
+        this.arrays = arrays
+        this.multipliers = []
+        let m = 1
         for (let i = arrays.length - 1; i >= 0; i--) {
-            this.multipliers[i] = m;
-            m *= arrays[i].length;
+            this.multipliers[i] = m
+            m *= arrays[i].length
         }
-        this.total = m;
-        this.available = Array.from({ length: this.total }, (_, i) => i);
+        this.total = m
+        this.available = Array.from({ length: this.total }, (_, i) => i)
         console.log('Всего людей:', this.total)
     }
 
     newPerson() {
-        if (this.available.length === 0) return null;
-        const randPoolIdx = Math.floor(Math.random() * this.available.length);
-        const comboIdx = this.available[randPoolIdx];
-        this.available[randPoolIdx] = this.available[this.available.length - 1];
-        this.available.pop();
-        let result = [];
-        let remainder = comboIdx;
+        if (this.available.length === 0) return null
+        const randPoolIdx = Math.floor(Math.random() * this.available.length)
+        const comboIdx = this.available[randPoolIdx]
+        this.available[randPoolIdx] = this.available[this.available.length - 1]
+        this.available.pop()
+        let result = []
+        let remainder = comboIdx
         for (let i = 0; i < this.arrays.length; i++) {
-            const arrIdx = Math.floor(remainder / this.multipliers[i]);
-            result.push(this.arrays[i][arrIdx]);
-            remainder %= this.multipliers[i];
+            const arrIdx = Math.floor(remainder / this.multipliers[i])
+            result.push(this.arrays[i][arrIdx])
+            remainder %= this.multipliers[i]
         }
         const sex = result[1][0]
         result[1] = result[1].replace(sex, '')
@@ -65,202 +74,117 @@ class Persones {
             result[0] = result[0].replace(/m[^w]*/, '').replace(/w/, '')
             result[2] = result[2].replace(/ич$/, 'на')
         }
-const numpass = comboIdx.toString().padStart(6, '0')
-const firstIndex = Math.floor(Math.random() * uniletters.length);
-  let secondIndex;
-  do {
-    secondIndex = Math.floor(Math.random() * uniletters.length);
-  } while (secondIndex === firstIndex);
-
- const prefix = uniletters[firstIndex] + uniletters[secondIndex]
- 
-        return { genre: sex, pass: comboIdx, numpass: `${prefix} ${numpass}`, fio: result.join(' ') };
+        const numpass = comboIdx.toString().padStart(6, '0')
+        const firstIndex = Math.floor(Math.random() * uniletters.length)
+        let secondIndex
+        do {
+            secondIndex = Math.floor(Math.random() * uniletters.length)
+        } while (secondIndex === firstIndex)
+        const prefix = uniletters[firstIndex] + uniletters[secondIndex]
+        return { genre: sex, pass: comboIdx, numpass: `${prefix} ${numpass}`, fio: result.join(' ') }
     }
 }
 
-const months = [
-    'Января',
-    'Февраля',
-    'Марта',
-    'Апреля',
-    'Мая',
-    'Июня',
-    'Июля',
-    'Августа',
-    'Сентября',
-    'Октября',
-    'Ноября',
-    'Декабря'
-]
-let hideCard = null
-let worldRunning = true, timespeed = 1
+//=========================================================================================================================
+const allpeople = [], maxClients = 7
+const clients = new Persones(data)
+
 document.getElementById('startBtn').classList.toggle('active')
 updateClock()
-const allpeople = [], maxClients = 7
-const clients = new Persones(data);
-
 for (let i = 0; i < maxClients; i++) {
-    const newpers = new Peoples();
+    const newpers = new Peoples()
     allpeople.push(newpers)
 }
 //allpeople[0].bank.acc = genNumAcc(allpeople[0].body.pass, 12)
 //allpeople[0].bank.balance = 100000000
 
-const peopleList =
-    document.getElementById('peopleList');
-
-const personCard =
-    document.getElementById('personCard');
+const peopleList = document.getElementById('peopleList')
+const personCard = document.getElementById('personCard')
 createPeopleList()
 
 const worldtime = setInterval(() => { worldtick() }, 1000)
 
-document.getElementById('pauseBtn').onclick = () => {
-    document.getElementById('pauseBtn').classList.toggle('active')
-    document.getElementById('startBtn').classList.remove('active')
-    document.getElementById('addTimeBtn').classList.remove('active')
-    worldRunning = false;
+const pauseBtn = document.getElementById('pauseBtn')
+const startBtn = document.getElementById('startBtn')
+const addTimeBtn = document.getElementById('addTimeBtn')
+pauseBtn.onclick = () => {
+    pauseBtn.classList.toggle('active')
+    startBtn.classList.remove('active')
+    addTimeBtn.classList.remove('active')
+    worldRunning = false
 }
 
-document.getElementById('startBtn').onclick = () => {
-    document.getElementById('pauseBtn').classList.remove('active')
-    document.getElementById('startBtn').classList.toggle('active')
-    document.getElementById('addTimeBtn').classList.remove('active')
+startBtn.onclick = () => {
+    pauseBtn.classList.remove('active')
+    startBtn.classList.toggle('active')
+    addTimeBtn.classList.remove('active')
     timespeed = 1
-    worldRunning = true;
+    worldRunning = true
 }
 
-document.getElementById('addTimeBtn').onclick = () => {
-    document.getElementById('pauseBtn').classList.remove('active')
-    document.getElementById('startBtn').classList.remove('active')
-    document.getElementById('addTimeBtn').classList.toggle('active')
+addTimeBtn.onclick = () => {
+    pauseBtn.classList.remove('active')
+    startBtn.classList.remove('active')
+    addTimeBtn.classList.toggle('active')
     timespeed = 10
-    worldRunning = true;
+    worldRunning = true
 }
 
 function genNumAcc(x, N = 6) {
-    const M = 10n ** BigInt(N);
-    const A = 742938281n;
-    const B = 1234567n;
-    const pseudoInt = (BigInt(x) * A + B) % M;
-    return pseudoInt.toString().padStart(N, '0').match(/.{1,4}/g).join(' ');
+    const M = 10n ** BigInt(N)
+    const A = 742938281n
+    const B = 1234567n
+    const pseudoInt = (BigInt(x) * A + B) % M
+    return pseudoInt.toString().padStart(N, '0').match(/.{1,4}/g).join(' ')
 }
 
 function createPeopleList() {
-
-    peopleList.innerHTML = '';
-
-    allpeople.forEach((person, index) => {
-
-        const button =
-            document.createElement('button');
-
-        button.className = 'person-button';
-
-        button.textContent =
-            person.body.fio;
-
+    peopleList.innerHTML = ''
+    allpeople.forEach(person => {
+        const button = document.createElement('button')
+        button.className = 'person-button'
+        button.textContent = person.body.fio
         button.onclick = () => {
-            showPerson(person);
-        };
-
-        peopleList.appendChild(button);
-    });
+            showPerson(person)
+        }
+        peopleList.appendChild(button)
+    })
 }
 
-
-function showPerson(person) {
-    if (hideCard){
-       clearTimeout(hideCard)
-    }
-
-    const buttons = document.querySelectorAll('.person-button');
-    buttons.forEach(b => b.innerText === person.body.fio?b.classList.toggle('active'):b.classList.remove('active'));
-   
-    const data = person;
-
-    // Пол
-    document.getElementById('personGenre')
-        .textContent =
-        data.body.genre === 'm'
-            ? '👨🏻'
-            : '👩🏻';
-
-
-    // Паспорт
-    document.getElementById('personPass')
-        .textContent =
-        `${data.body.numpass} ${data.evtm.toLocaleString()}`;
-
-
-    // ФИО
-    const fio =
-        data.body.fio.trim().split(/\s+/);
-
-    document.getElementById('personFio')
-        .innerHTML = `
-                    <span class="fio-first">
-                        ${fio[0]}
-                    </span>
-                    <span class="fio-rest">
-                        ${fio.slice(1).join(' ')}
-                    </span>
-                `;
-
-// Наличные
-    document.getElementById('personCash').textContent = '💵 '+
-        data.cash.toLocaleString('ru-RU')+' ₴';
-
-
-    // Банковский счёт
-    const bankInfo =
-        document.getElementById('bankInfo');
-
+function showPerson(data) {
+    if (hideCard) { clearTimeout(hideCard) }
+    const buttons = document.querySelectorAll('.person-button')
+    buttons.forEach(b => b.innerText === data.body.fio ? b.classList.toggle('active') : b.classList.remove('active'))
+    document.getElementById('personGenre').textContent = data.body.genre === 'm' ? '👨🏻' : '👩🏻'
+    document.getElementById('personPass').textContent = `${data.body.numpass} ${data.evtm.toLocaleString()}`
+    const fio = data.body.fio.trim().split(/\s+/)
+    document.getElementById('personFio').innerHTML = `<span class='fio-first'>${fio[0]}</span><span class='fio-rest'>${fio.slice(1).join(' ')}</span>`
+    document.getElementById('personCash').textContent = '💵 ' + data.cash.toLocaleString('ru-RU') + ' ₴'
+    const bankInfo = document.getElementById('bankInfo')
     if (data.bank && data.bank.acc !== null) {
-
-        bankInfo.style.display = 'block';
-
-        document.getElementById('personAccount')
-            .textContent = data.bank.acc;
-
-        document.getElementById('personBalance')
-            .textContent = '🏦 '+
-                data.bank.balance.toLocaleString('ru-RU')+' ₴';
-
+        bankInfo.style.display = 'block'
+        document.getElementById('personAccount').textContent = data.bank.acc
+        document.getElementById('personBalance').textContent = '🏦 ' + data.bank.balance.toLocaleString('ru-RU') + ' ₴'
     } else {
-
-        bankInfo.style.display = 'none';
+        bankInfo.style.display = 'none'
     }
-
-    personCard.classList.remove('hidden');
-    hideCard = setTimeout(()=>{
-       personCard.classList.toggle('hidden');
-       buttons.forEach(b => b.classList.remove('active'));
-    },60000)
+    personCard.classList.remove('hidden')
+    hideCard = setTimeout(() => {
+        personCard.classList.toggle('hidden')
+        buttons.forEach(b => b.classList.remove('active'))
+    }, 60000)
 }
 
 function worldtick() {
-    if (!worldRunning) {
-        return;
-    }
+    if (!worldRunning) return
     curTime.setMinutes(curTime.getMinutes() + timespeed)
-    allpeople.forEach((person, index) => {person.chgMoney()})
+    allpeople.forEach(person => { person.chgMoney() })
     updateClock()
-console.log('----------')
 }
 
 function updateClock() {
-    const hours =
-        String(curTime.getHours()).padStart(2, '0')
-
-    const minutes =
-        String(curTime.getMinutes()).padStart(2, '0')
-
-    document.getElementById('clock').textContent =
-        `${hours}:${minutes}`
-
-    document.getElementById('date').textContent =
-        `${curTime.getDate()} ` +
-        `${months[curTime.getMonth()]} ` +
-        `${curTime.getFullYear()}`
+    const hours = String(curTime.getHours()).padStart(2, '0')
+    const minutes = String(curTime.getMinutes()).padStart(2, '0')
+    document.getElementById('clock').textContent = `${hours}:${minutes}`
+    document.getElementById('date').textContent = `${curTime.getDate()} ${months[curTime.getMonth()]} ${curTime.getFullYear()}`
 }
